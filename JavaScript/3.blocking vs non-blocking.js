d8fbab1afff6f883e4e899139507f35898a18988
NAM TAE SIK <tesnam1216@gmail.com>
2021-09-04 23:43:56 +0900

    async, await 및 각 파일 번호붙임

--- JavaScript/3.blocking vs non-blocking.js
+++ JavaScript/3.blocking vs non-blocking.js
@@ -1,4 +1,6 @@
-//blocking
+// blocking (동기적 실행)
+// 호출된 함수가 자신이 할 일을 모두 마칠 때까지 제어권을 계속 가지고서 호출한 함수에게 바로 돌려주지 않으면 Block
+
 
 function waitSync(md) {
   let star = Date.now();
@@ -33,7 +35,9 @@ customers.forEach(function (customer) {
 });
 
 
-// non-blocking
+// non-blocking (비동기적 실행)
+// 호출된 함수가 자신이 할 일을 채 마치지 않았더라도 바로 제어권을 건네주어(return) 호출한 함수가 다른 일을 진행할 수 있도록 해주면 Non-block
+
 
 function waitAsync(callback, ms) {
   setTimeout(callback, ms); // 특정 시간 이후에 callback 함수가 실행되게끔 하는 브라우저 내장 기능
