05a32f0cf3e06f29faf9fe3c9b3474e27ff89fb9
Southbig <tesnam1216@gmail.com>
2022-11-19 16:53:21 +0900

    react hook study

---
+++ React/hook/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import HookTest from './components/hookTest';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hallo App</h1>
+      <HookTest />
+    </div>
+  );
+}
+
+export default App;
